770f1b1a7a6c519b5fc9de47a622d8c86f3e8816
Jayson Grace <jayson.e.grace@gmail.com>
2015-07-03 14:15:31 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-set -e
-
 ## install.sh
 ##
 ## Install all of the prerequisites with the exception of vagrant and virtualbox.
 ##
 ## Jayson Grace (jayson.e.grace@gmail.com)
+set -e
 
 gem install librarian-puppet --no-ri --no-rdoc
 librarian-puppet install
